7185b1cb2d63371315c79acc69c4701faa5ec989
abhijai001 <abhijaiadditional@gmail.com>
2026-08-29 19:42:26 +0530

    Initial commit

---
+++ lec1.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+name="John Doe";
+console.log(name);
+//there is nothing inside x;
+x=null;
+console.log(x);
+//don't know what inside y;
+y=undefined;
+console.log(y);
+//boolean type 
+isfollow=true;
+console.log(isfollow);
+//all together const objects
+//non primitive data type
+const student={
+    name:"abhijeet",
+    age:20,
+    cgpa:9.5,
+    isPass:true,
+};
+console.log(student["name"]);
+console.log(student["age"]);
+console.log(student["cgpa"]);
+console.log(student["isPass"]); 
